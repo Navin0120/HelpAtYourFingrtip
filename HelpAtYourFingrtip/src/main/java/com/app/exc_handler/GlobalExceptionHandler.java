34b66ef8a03f0fa17b0f399820f9891066fb1d4e
Navin <abc@gmail.com>
2022-04-10 22:35:05 +0530

    globalexchandler

--- HelpAtYourFingrtip/src/main/java/com/app/exc_handler/GlobalExceptionHandler.java
+++ HelpAtYourFingrtip/src/main/java/com/app/exc_handler/GlobalExceptionHandler.java
@@ -17,12 +17,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.app.custom_exceptions.WrongInputException;
 import com.app.dto.ErrorResponse;
 
-@ControllerAdvice // Mandatory annotation to tell SC , follo. class will contain common advice on
-					// exc handling
-//for all controllers
+@ControllerAdvice // Mandatory annotation to tell SC , following class will contain 
+				 //  common advice on exception handling for all controllers
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	//ResponseEntityExceptionHandler -> A convenient base class for @ControllerAdvice classes that wish to provide 
+	//centralized exception handling across all @RequestMapping methods through @ExceptionHandler methods
 
-	//how to handle globally , validation related errors ?
+	//Handle validation related errors globally
+	//ResponseEntityExceptionHandler.class => @ExceptionHandler(MethodArgumentNotValidException.class1)
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -31,6 +34,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		.collect(Collectors.toMap(FieldError::getField,FieldError::getDefaultMessage));
 		return ResponseEntity.badRequest().body(map);
 	}
+	//Exception 		--> checked --> force to handle
+	//RuntimeException	--> unchecked --> not forced to handle
 	//how to deal with custom exc globally ? 
 	//how to tell SC , following is an exc handling method
 	//method arg : Class<T> : T => type of the exception , to be handled
@@ -43,12 +48,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		//dto to create error response
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resp);
 	}	
+	//INTERNAL_SERVER_ERROR => 500 => server side issue eg. permission error,3rd party plugin not available 
+	//BAD_REQUEST => 400 => bad client request => eg.id not found
+	//CREATED => 201
+	//OK => 200
+	//NOT_FOUND => 404
+	//ACCEPTED => 202
+	//NO_CONTENT => 204
+
+	/*	Informational responses (100–199)
+		Successful responses (200–299)
+		Redirection messages (300–399)
+		Client error responses (400–499)
+		Server error responses (500–599) */
+	
 	@ExceptionHandler(WrongInputException.class)
 	public ResponseEntity<?> handleWongInputException(RuntimeException e)
 	{
 		ErrorResponse resp=new ErrorResponse(e.getMessage(),LocalDateTime.now());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resp);
-	}
+	}	
 	
-
+	
 }
